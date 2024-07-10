93e9be0e0c0670854239ff3380a2af1374d9524e
Emanuele <egalli64@gmail.com>
2024-07-10 12:15:48 +0200

    new slide

--- src/main/java/com/example/jade/s14/Main.java
+++ src/main/java/com/example/jade/s14/Main.java
@@ -1,4 +1,9 @@
-package com.example.jade.s13;
+/*
+ * Introduction to Java Design Principles and Patterns
+ * 
+ * https://github.com/egalli64/jade
+ */
+package com.example.jade.s14;
 
 import java.util.Iterator;
 
